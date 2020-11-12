ee6f4ddd7316a7b47d9b9ed597ccf4808ad9453c
bayuagung100 <bayuagung100@gmail.com>
2020-11-12 19:33:42 +0700

    first commit

---
+++ Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text } from 'react-native';
+
+
+// component
+class Header extends Component{
+    render(){
+      return(
+        <Text>Ini Header</Text>
+      )
+    }
+}
+  
+export default Header;
